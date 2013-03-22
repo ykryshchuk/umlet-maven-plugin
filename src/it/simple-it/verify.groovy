93dd2d88c71ab418ee8c6b245c0fc6b7072ba982
Yuriy Kryshchuk <yuriy.kryshchuk@gmail.com>
2013-03-22 13:50:06 +0200

    Switch to file visitor

--- src/it/simple-it/verify.groovy
+++ src/it/simple-it/verify.groovy
@@ -1,3 +1,9 @@
-File touchFile = new File( basedir, "target/site/uml/simple.png" );
+File simpleFile = new File( basedir, "target/site/uml/simple.png" );
+assert simpleFile.isFile()
 
-assert touchFile.isFile()
+File compAFile = new File( basedir, "target/site/uml/comp/compA.png" );
+assert compAFile.isFile()
+
+File compBFile = new File( basedir, "target/site/uml/comp/compB.png" );
+assert compBFile.isFile()
+
--- src/main/java/com/kryshchuk/maven/plugins/umlet/ConvertDiagramMojo.java
+++ src/main/java/com/kryshchuk/maven/plugins/umlet/ConvertDiagramMojo.java
@@ -1,8 +1,9 @@
 package com.kryshchuk.maven.plugins.umlet;
 
 import java.io.File;
-import java.io.FileFilter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -10,33 +11,24 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
 import com.baselet.diagram.DiagramHandler;
+import com.kryshchuk.maven.plugins.filevisitor.AbstractFileVisitor;
+import com.kryshchuk.maven.plugins.filevisitor.FileMapper;
+import com.kryshchuk.maven.plugins.filevisitor.FileSet;
+import com.kryshchuk.maven.plugins.filevisitor.FileSetIterator;
+import com.kryshchuk.maven.plugins.filevisitor.FileVisitor;
+import com.kryshchuk.maven.plugins.filevisitor.ReplaceExtensionFileMapper;
+import com.kryshchuk.maven.plugins.filevisitor.VisitorException;
 
 /**
  * Converts the UMLet UXF diagrams into the graphics files.
  * 
  * @since 1.0.0
  */
-@Mojo(name = "convert", defaultPhase = LifecyclePhase.PRE_SITE, threadSafe = true)
+@Mojo(name = "convert", defaultPhase = LifecyclePhase.PRE_SITE, threadSafe = true, requiresProject = true)
 public class ConvertDiagramMojo extends AbstractMojo {
-
-  private static final FileFilter DIAGRAM_FILEFILTER = new FileFilter() {
-
-    /**
-     * @return
-     */
-    public boolean accept(final File f) {
-      if (f.isDirectory()) {
-        return true;
-      }
-      if (f.getName().toLowerCase().endsWith(".uxf")) {
-        return true;
-      }
-      return false;
-    }
-
-  };
 
   /**
    * Target directory to store the converted images. If the source directory contains the sub-directories with diagram
@@ -50,13 +42,8 @@ public class ConvertDiagramMojo extends AbstractMojo {
   @Parameter(defaultValue = "${project.reporting.outputDirectory}/uml", property = "umlet.targetDir", required = true)
   private File outputDirectory;
 
-  /**
-   * Root source directory with UMLet UXF diagrams. Sub-directories will be scanned for the diagram sources as well.
-   * 
-   * @since 1.0.0
-   */
-  @Parameter(defaultValue = "${basedir}/src/site/resources/uml", property = "umlet.sourceDir", required = true)
-  private File sourceDirectory;
+  @Parameter(required = false)
+  private List<FileSet> filesets;
 
   /**
    * Convert diagram to this image format. Supported formats are <code>bmp</code>, <code>eps</code>, <code>gif</code>,
@@ -66,6 +53,30 @@ public class ConvertDiagramMojo extends AbstractMojo {
    */
   @Parameter(defaultValue = "png", property = "umlet.format", required = true)
   private ConvertFormat format;
+
+  @Parameter(defaultValue = "${project}", required = true, readonly = true)
+  private MavenProject mavenProject;
+
+  private final FileVisitor visitor = new AbstractFileVisitor() {
+
+    @Override
+    protected boolean shouldOverwrite(final File inputFile, final File outputFile) {
+      return false;
+    }
+
+    @Override
+    protected void handleFile(final File inputFile, final File outputFile) throws VisitorException {
+      verifyParentDirectory(outputFile);
+      final DiagramHandler diagramHandler = new DiagramHandler(inputFile);
+      try {
+        getLog().info("Converting " + inputFile);
+        diagramHandler.getFileHandler().doExportAs(format.name(), outputFile);
+      } catch (final IOException e) {
+        getLog().error("Failed to convert diagram", e);
+        throw new VisitorException("Cannot convert diagram " + inputFile, e);
+      }
+    }
+  };
 
   /**
    * <p>
@@ -79,68 +90,41 @@ public class ConvertDiagramMojo extends AbstractMojo {
    * @since 1.0.8
    */
   public void execute() throws MojoExecutionException, MojoFailureException {
-    getLog().debug("Diagram directory: " + sourceDirectory);
-    getLog().debug("Output directory: " + outputDirectory);
-    getLog().debug("Format: " + format);
-    if (sourceDirectory.isDirectory()) {
-      processDirectory(sourceDirectory);
+    if (filesets == null || filesets.isEmpty()) {
+      getLog().info("Using default diagrams location");
+      processFileSet(new DefaultDiagramSet());
     } else {
-      getLog().warn("Diagram directory does not exist: " + sourceDirectory);
-    }
-  }
-
-  private void processDirectory(final File dir) throws MojoExecutionException, MojoFailureException {
-    final File[] files = sourceDirectory.listFiles(DIAGRAM_FILEFILTER);
-    for (final File f : files) {
-      if (f.isDirectory()) {
-        processDirectory(f);
-      } else {
-        processDiagram(f);
+      for (final FileSet fs : filesets) {
+        processFileSet(fs);
       }
     }
   }
 
-  private void processDiagram(final File diagram) throws MojoExecutionException, MojoFailureException {
-    final String diagramFilepath = diagram.getAbsolutePath();
-    final String sourceDirpath = sourceDirectory.getAbsolutePath();
-    if (diagramFilepath.startsWith(sourceDirpath)) {
-      final String relPath = diagramFilepath.substring(sourceDirpath.length());
-      final int extIndex = relPath.lastIndexOf('.');
-      final String targetRel = relPath.substring(0, extIndex) + "." + format;
-      getLog().debug("Found source diagram " + targetRel);
-      final File imageFile = new File(outputDirectory, targetRel);
-      if (!imageFile.isFile() || diagram.lastModified() > imageFile.lastModified()) {
-        checkOutputDirectory(imageFile.getParentFile());
-        final DiagramHandler diagramHandler = new DiagramHandler(diagram);
-        try {
-          getLog().info("Converting " + diagram);
-          diagramHandler.getFileHandler().doExportAs(format.name(), imageFile);
-        } catch (final IOException e) {
-          getLog().error("Failed to convert diagram", e);
-          throw new MojoFailureException(this, "Cannot convert diagram " + diagram.getAbsolutePath(), e.getMessage());
-        }
-      } else {
-        getLog().debug("Diagram image is up to date: " + imageFile.getAbsolutePath());
-      }
-    } else {
-      throw new MojoExecutionException("Could not determine target image path");
+  /**
+   * @param fileSet
+   *          fileset to process
+   * @throws MojoExecutionException
+   */
+  private void processFileSet(final FileSet fileSet) throws MojoExecutionException {
+    final FileMapper fileMapper = new ReplaceExtensionFileMapper(fileSet.getDirectory(), outputDirectory, format.name());
+    final FileSetIterator i = new FileSetIterator(fileSet, fileMapper);
+    try {
+      i.iterate(visitor);
+    } catch (final VisitorException e) {
+      throw new MojoExecutionException("Failed to iterate fileset", e);
     }
   }
 
-  private void checkOutputDirectory(final File dir) throws MojoFailureException {
-    if (dir.isDirectory()) {
-      getLog().debug("Output directory exists: " + dir.getAbsolutePath());
-    } else {
-      if (dir.exists()) {
-        throw new MojoFailureException("Output destination exists, but is not a directory");
-      } else {
-        if (dir.mkdirs()) {
-          getLog().debug("Created output directory: " + dir.getAbsolutePath());
-        } else {
-          throw new MojoFailureException("Could not create output directory");
-        }
-      }
-    }
-  }
+  private class DefaultDiagramSet extends FileSet {
 
+    /**
+     * 
+     */
+    DefaultDiagramSet() {
+      final File diagramsDirectory = new File(mavenProject.getBasedir(), "src/site/resources/uml");
+      setDirectory(diagramsDirectory);
+      setIncludes(Arrays.asList("**/*.uxf"));
+    }
+
+  }
 }
